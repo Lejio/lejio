4eb11988e0f8c30197ae1808ee606be5c6477309
Gene Desktop <geneisjuan@gmail.com>
2024-09-17 19:23:07 -0400

    udpated timeline and created view transition animations for all professional experiences

--- src/components/ui/TimelineItem.tsx
+++ src/components/ui/TimelineItem.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "../../lib/hooks";
 
-export default function TimelineItem({ children, index, href }: {children: React.ReactNode, index: number, href: string}) {
+export default function TimelineItem({ children, index }: {children: React.ReactNode, index: number}) {
 
   const ref = useRef<HTMLLIElement>(null); // Correctly type the ref
   const isInView = useInView(ref);
@@ -15,18 +15,16 @@ export default function TimelineItem({ children, index, href }: {children: React
   }, [isInView, controls]);
 
   return (
-    <a href={href}>
-      <motion.li
-        key={index}
-        ref={ref} // Correctly assign the ref to the motion component
-        // className="mb-10 "
-        initial={{ opacity: 0, y: 50 }}
-        animate={controls}
-        transition={{ duration: 0.5, delay: index * 0.2 }}
-      >
-        { children }
-      </motion.li>
-    </a>
-  )
+    <motion.li
+      key={index}
+      ref={ref} // Correctly assign the ref to the motion component
+      className=" max-w-[1000px]"
+      initial={{ opacity: 0, y: 50 }}
+      animate={controls}
+      transition={{ duration: 0.5, delay: index * 0.2 }}
+    >
+      { children }
+    </motion.li>
+)
 }
 
