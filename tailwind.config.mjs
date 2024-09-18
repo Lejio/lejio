6fe3b0bdc79daa49f978c917d26760f0231aa6a4
Gene Desktop <geneisjuan@gmail.com>
2024-09-17 21:38:58 -0400

    fixed theme switching not working by adding darkMode option in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,7 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class', // or 'media' or 'class'
 	theme: {
 		extend: {
 			FontFamily: {
